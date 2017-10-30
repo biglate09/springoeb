12e46fc5032f6e6cdc1bea0ed5ea9b8009b21d32
DESKTOP-BPS46Q7\USER <biglate_09@hotmail.com>
2017-10-30 18:12:01 +0700

    ทำงานต่อได้

--- src/test/java/com/springoeb/SpringoebApplicationTests.java
+++ src/test/java/com/springoeb/SpringoebApplicationTests.java
@@ -2,6 +2,7 @@ package com.springoeb;
 
 import com.springoeb.cashier.service.OrderService;
 import com.springoeb.menu.service.BranchMenuService;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,9 +11,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringoebApplicationTests {
-
 	@Autowired
 	private OrderService orderService;
 	@Autowired
 	private BranchMenuService branchMenuService;
+
+	@Test
+	public void setupTest() {
+	}
 }
